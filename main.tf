b94c5edd39063e615dd4e6d2b6bb8f7be47b921b
hemanth-chittela <hemanth1337@outlook.com>
2023-12-30 09:19:32 +0530

    new files

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
+      name = "frontend"
 
       instance_types = ["t3.small"]
 
@@ -78,7 +78,16 @@ module "eks" {
     }
 
     two = {
-      name = "node-group-2"
+      name = "spring"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+    three = {
+      name = "database"
 
       instance_types = ["t3.small"]
 
